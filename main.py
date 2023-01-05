c5cf4b2875d99f0cbd2979d191d99c3368cd9190
Abhinav Menon <abhinav.m@research.iiit.ac.in>
2023-01-05 05:34:46 +0530

    Update model names, example

--- main.py
+++ main.py
@@ -4,29 +4,44 @@ classes = [('nonlin', ToyRNN),
            ('linear', LinRNN),
            ('tied', TieRNN)]
 
-descriptions = [('7-10:Nonlin:True', 'models/in=7-out=10-nonlin-offset=0.pkl'),
-                ('7-10:Nonlin:False', 'models/in=7-out=10-nonlin-offset=0-bias=False.pkl'),
-                ('7-10:Linear:True', 'models/in=7-out=10-linear-offset=0.pkl'),
-                ('7-10:Linear:False', 'models/in=7-out=10-linear-offset=0-bias=False.pkl'),
-                ('7-10:Tied:True', 'models/in=7-out=10-tied-offset=0-bias=True.pkl'),
-                ('7-10:Tied:False', 'models/in=7-out=10-tied-offset=0-bias=False.pkl'),
-                ('7-10:Tied:Tied', 'models/in=7-out=10-tied-offset=0-bias=False.pkl'),
+descriptions = [('7-10:Nonlin:True:v1', 'models/in=7-out=10-nonlin-bias=True-offset=0-v1.ckpt'),
+                ('7-10:Nonlin:True:v2', 'models/in=7-out=10-nonlin-bias=True-offset=0-v2.ckpt'),
+                ('7-10:Nonlin:False:v1', 'models/in=7-out=10-nonlin-bias=False-offset=0-v1.ckpt'),
+                ('7-10:Nonlin:False:v2', 'models/in=7-out=10-nonlin-bias=False-offset=0-v2.ckpt'),
+                ('7-10:Linear:True:v1', 'models/in=7-out=10-linear-bias=True-offset=0-v1.ckpt'),
+                ('7-10:Linear:True:v2', 'models/in=7-out=10-linear-bias=True-offset=0-v2.ckpt'),
+                ('7-10:Linear:False:v1', 'models/in=7-out=10-linear-bias=False-offset=0-v1.ckpt'),
+                ('7-10:Linear:False:v2', 'models/in=7-out=10-linear-bias=False-offset=0-v2.ckpt'),
+                ('7-10:Tied:True:v1', 'models/in=7-out=10-tied-bias=True-offset=0-v1.ckpt'),
+                ('7-10:Tied:True:v2', 'models/in=7-out=10-tied-bias=True-offset=0-v2.ckpt'),
+                ('7-10:Tied:False:v1', 'models/in=7-out=10-tied-bias=False-offset=0-v1.ckpt'),
+                ('7-10:Tied:False:v2', 'models/in=7-out=10-tied-bias=False-offset=0-v2.ckpt'),
 
-                ('7-7:Nonlin:True', 'models/in=7-out=7-nonlin-offset=0.pkl'),
-                ('7-7:Nonlin:False', 'models/in=7-out=7-nonlin-offset=0-bias=False.pkl'),
-                ('7-7:Linear:True', 'models/in=7-out=7-linear-offset=0.pkl'),
-                ('7-7:Linear:False', 'models/in=7-out=7-linear-offset=0-bias=False.pkl'),
-                ('7-7:Tied:True', 'models/in=7-out=7-tied-offset=0-bias=True.pkl'),
-                ('7-7:Tied:False', 'models/in=7-out=7-tied-offset=0-bias=False.pkl'),
-                ('7-7:Tied:Tied', 'models/in=7-out=7-tied-offset=0-bias=False.pkl'),
+                ('7-7:Nonlin:True:v1', 'models/in=7-out=7-nonlin-bias=True-offset=0-v1.ckpt'),
+                ('7-7:Nonlin:True:v2', 'models/in=7-out=7-nonlin-bias=True-offset=0-v2.ckpt'),
+                ('7-7:Nonlin:False:v1', 'models/in=7-out=7-nonlin-bias=False-offset=0-v1.ckpt'),
+                ('7-7:Nonlin:False:v2', 'models/in=7-out=7-nonlin-bias=False-offset=0-v2.ckpt'),
+                ('7-7:Linear:True:v1', 'models/in=7-out=7-linear-bias=True-offset=0-v1.ckpt'),
+                ('7-7:Linear:True:v2', 'models/in=7-out=7-linear-bias=True-offset=0-v2.ckpt'),
+                ('7-7:Linear:False:v1', 'models/in=7-out=7-linear-bias=False-offset=0-v1.ckpt'),
+                ('7-7:Linear:False:v2', 'models/in=7-out=7-linear-bias=False-offset=0-v2.ckpt'),
+                ('7-7:Tied:True:v1', 'models/in=7-out=7-tied-bias=True-offset=0-v1.ckpt'),
+                ('7-7:Tied:True:v2', 'models/in=7-out=7-tied-bias=True-offset=0-v2.ckpt'),
+                ('7-7:Tied:False:v1', 'models/in=7-out=7-tied-bias=False-offset=0-v1.ckpt'),
+                ('7-7:Tied:False:v2', 'models/in=7-out=7-tied-bias=False-offset=0-v2.ckpt'),
 
-                ('7-5:Nonlin:True', 'models/in=7-out=5-nonlin-offset=0.pkl'),
-                ('7-5:Nonlin:False', 'models/in=7-out=5-nonlin-offset=0-bias=False.pkl'),
-                ('7-5:Linear:True', 'models/in=7-out=5-linear-offset=0.pkl'),
-                ('7-5:Linear:False', 'models/in=7-out=5-linear-offset=0-bias=False.pkl'),
-                ('7-5:Tied:True', 'models/in=7-out=5-tied-offset=0-bias=True.pkl'),
-                ('7-5:Tied:False', 'models/in=7-out=5-tied-offset=0-bias=False.pkl'),
-                ('7-5:Tied:Tied', 'models/in=7-out=5-tied-offset=0-bias=False.pkl')]
+                ('7-5:Nonlin:True:v1', 'models/in=7-out=5-nonlin-bias=True-offset=0-v1.ckpt'),
+                ('7-5:Nonlin:True:v2', 'models/in=7-out=5-nonlin-bias=True-offset=0-v2.ckpt'),
+                ('7-5:Nonlin:False:v1', 'models/in=7-out=5-nonlin-bias=False-offset=0-v1.ckpt'),
+                ('7-5:Nonlin:False:v2', 'models/in=7-out=5-nonlin-bias=False-offset=0-v2.ckpt'),
+                ('7-5:Linear:True:v1', 'models/in=7-out=5-linear-bias=True-offset=0-v1.ckpt'),
+                ('7-5:Linear:True:v2', 'models/in=7-out=5-linear-bias=True-offset=0-v2.ckpt'),
+                ('7-5:Linear:False:v1', 'models/in=7-out=5-linear-bias=False-offset=0-v1.ckpt'),
+                ('7-5:Linear:False:v2', 'models/in=7-out=5-linear-bias=False-offset=0-v2.ckpt'),
+                ('7-5:Tied:True:v1', 'models/in=7-out=5-tied-bias=True-offset=0-v1.ckpt'),
+                ('7-5:Tied:True:v2', 'models/in=7-out=5-tied-bias=True-offset=0-v2.ckpt'),
+                ('7-5:Tied:False:v1', 'models/in=7-out=5-tied-bias=False-offset=0-v1.ckpt'),
+                ('7-5:Tied:False:v2', 'models/in=7-out=5-tied-bias=False-offset=0-v2.ckpt')]
 
 # General code to run, save, load and visualise model
 #toy = ToyRNN(7, 10, 0)
@@ -39,5 +54,5 @@ descriptions = [('7-10:Nonlin:True', 'models/in=7-out=10-nonlin-offset=0.pkl'),
 #trn = pl.Trainer(auto_lr_find=True, max_epochs=100, callbacks=[ckpt, es])
 #trn.tune(toy)
 #trn.fit(toy)
-#toy_reloaded = load_model_from_name('in=7-out=10-nonlin-bias=True-offset=0')
-#visualise_model(toy_reloaded, OffsetData(7, 0, 200, 1)[0][0])
+#toy_reloaded = load_model_from_name(ToyRNN, 'in=7-out=10-nonlin-bias=True-offset=0')
+#visualise_model(toy_reloaded)[0][0])
