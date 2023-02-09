aed526a5f61d270b0a5f7c15c6a2a86831f14932
Abhinav Menon <abhinav.m@research.iiit.ac.in>
2023-02-09 06:37:01 +0530

    MLM code (buggy)

---
+++ mlm/data.py
@@ -0,0 +1,48 @@
+from config import *
+
+class RegData(Dataset):
+    def __init__(self, regex, alphabet="0123abcd@M~", num_samples=20000):
+        self.regex = regex
+
+        self.i2v = alphabet
+        self.v2i = {c: i for i, c in enumerate(self.i2v)}
+
+        strings = []
+        masked_strings = []
+        max_length = 0
+        for _ in range(num_samples//2):
+            sample = exrex.getone(self.regex)
+            strings.append(sample)
+            masked_strings.append(sample)
+
+            max_length = max(max_length, len(sample))
+
+            mask_posns = random.choices(range(len(sample)), k=ceil(len(sample)*0.15))
+            for p in mask_posns:
+                x = list(masked_strings[-1])
+                x[p] = 'M'
+                masked_strings[-1] = ''.join(x)
+
+        self.strings = strings
+        
+        indices = []
+        masked_indices = []
+        pad_masks = []
+        pad_index = self.v2i['~'] # = len(self.v2i) - 1
+        for i in range(len(strings)):
+            sample = strings[i]
+            masked_sample = masked_strings[i]
+            pad_length = max_length-len(sample)
+            pad_masks.append([1]*len(sample) + [0]*pad_length)
+            indices.append([self.v2i[c] for c in sample] + [pad_index]*pad_length)
+            masked_indices.append([self.v2i[c] for c in masked_sample] + [pad_index]*pad_length)
+        
+        self.indices = tensor(indices)
+        self.masked_indices = tensor(masked_indices)
+        self.pad_masks = tensor(pad_masks)
+
+    def __getitem__(self, index):
+        return self.indices[index], self.masked_indices[index], self.pad_masks[index]
+    
+    def __len__(self):
+        return self.indices.shape[0]
---
+++ mlm/model.py
@@ -0,0 +1,76 @@
+from data import *
+
+class RegModel(pl.LightningModule):
+    def __init__(self, regex, num_layers, emb_dim, nhead, ff_dim, alphabet="0123abcd@M~"):
+        super().__init__()
+        self.save_hyperparameters(ignore=['lf'])
+        self.lf = nn.CrossEntropyLoss()
+        self.lr = 0.1
+
+        self.regex = regex
+        self.alphabet = alphabet
+
+        self.embedding = nn.Embedding(len(alphabet), emb_dim)
+        self.layer = nn.TransformerEncoderLayer(d_model=emb_dim, nhead=nhead,
+                                                dim_feedforward=ff_dim, batch_first=True)
+        self.encoder = nn.TransformerEncoder(self.layer, num_layers=num_layers)
+        self.decoder = nn.Linear(emb_dim, len(alphabet), bias=True)
+
+    def get_dataloader(self, num_samples=20000):
+        ds = RegData(self.regex, self.alphabet, num_samples=num_samples)
+        dl = DataLoader(ds, batch_size=BATCH_SIZE)
+        return dl
+
+    def step(self, loss_type, batch, batch_idx):
+        strings, masked_strings, pad_masks = batch
+        # [bz, sq]
+        reps = self(masked_strings, pad_masks)
+        # [bz, sq, d]
+        preds = self.decoder(reps)
+        # [bz, sq, v]
+
+        masked_tokens = strings * (masked_strings == 9)
+        masked_tokens = masked_tokens[masked_tokens > 0]
+
+        masked_preds = (preds * (masked_strings == 9).unsqueeze(2).repeat(1,1,len(self.alphabet)))
+        masked_preds = masked_preds[masked_preds != 0.0].view(-1, len(self.alphabet))
+
+        loss = self.lf(masked_preds, masked_tokens)
+
+        self.log(loss_type, loss, prog_bar=True)
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self.step("train_loss", batch, batch_idx)
+
+    def validation_step(self, batch, batch_idx):
+        return self.step("val_loss", batch, batch_idx)
+    
+    def test_step(self, batch, batch_idx):
+        strings, _, pad_masks = batch
+        reps = self(strings, pad_masks)
+        preds = self.decoder(reps)
+
+        loss = self.lf(preds.transpose(1,2), strings)
+        
+        self.log("test_loss", loss, prog_bar=True)
+        return loss
+
+    def train_dataloader(self):
+        return self.get_dataloader()
+
+    def val_dataloader(self):
+        return self.get_dataloader(num_samples=50000)
+    
+    def test_dataloader(self):
+        return self.get_dataloader(num_samples=50000)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+    def forward(self, strings, masks):
+                      # [bz, n]
+        embeddings = self.embedding(strings)
+        representations = self.encoder(src=embeddings, src_key_padding_mask=masks)
+        return representations
